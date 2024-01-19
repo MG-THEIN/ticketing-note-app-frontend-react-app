37551d0c10ea5f4550e2647e021d5ed7bf4cd04d
MG-THEIN <mt4130c@gmail.com>
2024-01-19 01:14:35 -0500

    fix deployment errors

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 import { setCredentials } from "../../features/auth/authSlice"
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://ticketing-note-app-api.onrender.com",
+  baseUrl: "https://ticketing-note-app-frontend-react-app.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token
